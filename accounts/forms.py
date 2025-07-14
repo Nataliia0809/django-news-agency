96d3c2ac2d5e6756d32ce27dfc319d66a0e3c7d0
Nataliia <nperepeliak7@gmail.com>
2025-07-14 14:50:22 +0300

    made a complete search (advanced with filters and simple), added ajax prompts when entering)

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,61 +7,62 @@ class RedactorCreationForm(UserCreationForm):
     first_name = forms.CharField(
         max_length=30,
         required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'First Name'
-        })
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "First Name"}
+        ),
     )
     last_name = forms.CharField(
         max_length=30,
         required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Last Name'
-        })
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Last Name"}
+        ),
     )
     email = forms.EmailField(
         required=True,
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Email'
-        })
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Email"}
+        ),
     )
     years_of_experience = forms.IntegerField(
         min_value=0,
         max_value=50,
         required=True,
-        widget=forms.NumberInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Years of Experience'
-        })
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Years of Experience"}
+        ),
     )
 
     class Meta:
         model = Redactor
-        fields = ('username', 'first_name', 'last_name', 'email', 'years_of_experience', 'password1', 'password2')
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "years_of_experience",
+            "password1",
+            "password2",
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Username'
-        })
-        self.fields['password1'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Password'
-        })
-        self.fields['password2'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Confirm Password'
-        })
+        self.fields["username"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Username"}
+        )
+        self.fields["password1"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Password"}
+        )
+        self.fields["password2"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Confirm Password"}
+        )
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.years_of_experience = self.cleaned_data['years_of_experience']
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.years_of_experience = self.cleaned_data["years_of_experience"]
         if commit:
             user.save()
         return user
@@ -70,20 +71,12 @@ class RedactorCreationForm(UserCreationForm):
 class RedactorUpdateForm(forms.ModelForm):
     class Meta:
         model = Redactor
-        fields = ['first_name', 'last_name', 'email', 'years_of_experience']
+        fields = ["first_name", "last_name", "email", "years_of_experience"]
         widgets = {
-            'first_name': forms.TextInput(attrs={
-                'class': 'form-control'
-            }),
-            'last_name': forms.TextInput(attrs={
-                'class': 'form-control'
-            }),
-            'email': forms.EmailInput(attrs={
-                'class': 'form-control'
-            }),
-            'years_of_experience': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'min': '0',
-                'max': '50'
-            }),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "years_of_experience": forms.NumberInput(
+                attrs={"class": "form-control", "min": "0", "max": "50"}
+            ),
         }
--- news/views.py
+++ news/views.py
@@ -13,6 +13,9 @@ from django.contrib import messages
 from .models import Newspaper, Topic
 from .forms import NewspaperForm, TopicForm, RedactorForm
 from accounts.models import Redactor
+from django.db.models import Q, Count
+from django.core.paginator import Paginator
+from .forms import SearchForm
 
 
 # гол сторінка🔻
@@ -114,3 +117,137 @@ class RedactorDetailView(DetailView):
     model = Redactor
     template_name = "news/redactor_detail.html"
     context_object_name = "redactor"
+
+
+# 🔻пошук
+def search_view(request):
+    form = SearchForm(request.GET or None)
+    newspapers = Newspaper.objects.filter(is_published=True)
+    query = None
+
+    if form.is_valid():
+        # текстовий пошук
+        query = form.cleaned_data.get("query")
+        if query:
+            newspapers = newspapers.filter(
+                Q(title__icontains=query)
+                | Q(content__icontains=query)
+                | Q(excerpt__icontains=query)
+                | Q(topic__name__icontains=query)
+                | Q(publishers__first_name__icontains=query)
+                | Q(publishers__last_name__icontains=query)
+                | Q(publishers__username__icontains=query)
+            ).distinct()
+
+        # фільтр за темою
+        topic = form.cleaned_data.get("topic")
+        if topic:
+            newspapers = newspapers.filter(topic=topic)
+
+        # за автором
+        author = form.cleaned_data.get("author")
+        if author:
+            newspapers = newspapers.filter(publishers=author)
+
+        # по даті від
+        date_from = form.cleaned_data.get("date_from")
+        if date_from:
+            newspapers = newspapers.filter(published_date__gte=date_from)
+
+        # по даті до
+        date_to = form.cleaned_data.get("date_to")
+        if date_to:
+            newspapers = newspapers.filter(published_date__lte=date_to)
+
+        # по пріоритету
+        priority = form.cleaned_data.get("priority")
+        if priority:
+            newspapers = newspapers.filter(priority=priority)
+
+        # сортування
+        sort_by = form.cleaned_data.get("sort_by")
+        if sort_by:
+            newspapers = newspapers.order_by(sort_by)
+        elif query:  # якщо є пошуковий запит, сортую відповідно до нього
+            newspapers = newspapers.order_by("-published_date")
+        else:
+            newspapers = newspapers.order_by("-published_date")
+
+    # Пагінація
+    paginator = Paginator(
+        newspapers.select_related("topic").prefetch_related("publishers"), 12
+    )
+    page = request.GET.get("page")
+    newspapers_page = paginator.get_page(page)
+
+    context = {
+        "form": form,
+        "newspapers": newspapers_page,
+        "query": query,
+        "total_results": newspapers.count(),
+        "page_obj": newspapers_page,
+        "is_paginated": newspapers_page.has_other_pages(),
+    }
+
+    return render(request, "news/search_results.html", context)
+
+
+# AJAX (підказки, коли юзер вводить текст)
+from django.http import JsonResponse
+
+
+def search_autocomplete(request):
+    query = request.GET.get("q", "")
+    if len(query) < 2:
+        return JsonResponse({"suggestions": []})
+
+    # по заголовках
+    newspapers = Newspaper.objects.filter(title__icontains=query, is_published=True)[:5]
+
+    # по темах
+    topics = Topic.objects.filter(name__icontains=query, is_active=True)[:3]
+
+    # по авторах
+    authors = Redactor.objects.filter(
+        Q(first_name__icontains=query)
+        | Q(last_name__icontains=query)
+        | Q(username__icontains=query),
+        is_active_redactor=True,
+    )[:3]
+
+    suggestions = []
+
+    # додаємо новини
+    for newspaper in newspapers:
+        suggestions.append(
+            {
+                "type": "newspaper",
+                "title": newspaper.title,
+                "url": newspaper.get_absolute_url(),
+                "icon": "newspaper",
+            }
+        )
+
+    # додаємо теми
+    for topic in topics:
+        suggestions.append(
+            {
+                "type": "topic",
+                "title": f"Тема: {topic.name}",
+                "url": topic.get_absolute_url(),
+                "icon": "tag",
+            }
+        )
+
+    # додаю авторів
+    for author in authors:
+        suggestions.append(
+            {
+                "type": "author",
+                "title": f"Автор: {author.full_name or author.username}",
+                "url": reverse("news:redactor-detail", kwargs={"pk": author.pk}),
+                "icon": "user",
+            }
+        )
+
+    return JsonResponse({"suggestions": suggestions})
