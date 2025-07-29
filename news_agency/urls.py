9da7d08e10b123e823782ee61d20bbf5ad780972
Nataliia <nperepeliak7@gmail.com>
2025-07-30 01:04:15 +0300

    Add media URL redirect for production

--- news_agency/urls.py
+++ news_agency/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,3 +16,11 @@ if settings.DEBUG:
         path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # For production - serve media files through static files
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Redirect /media/ to /static/media/ for old URLs in database
+    urlpatterns += [
+        path('media/<path:path>', RedirectView.as_view(url='/static/media/%(path)s', permanent=True)),
+    ]
